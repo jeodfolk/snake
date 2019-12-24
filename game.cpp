83df03cf7639f24b0e1c6eaefd73ee9abdc0daa3
jeodfolk <woolfolkjoseph1@yahoo.com>
2019-12-24 01:05:28 -0700

    snake struct and movement handling

--- game.cpp
+++ game.cpp
@@ -7,7 +7,6 @@
 #include "game.h"
 
 using namespace std;
-#define KEY_ARROW_CHAR1 224
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
@@ -17,14 +16,22 @@ MainGame::MainGame()
     {
     mapHeight = 20;
     mapWidth = 50;
+    direction = 2;
     snakePos = (mapHeight*mapWidth)/2;
     map = new char [20*50];
+
+    Snake *firstSeg = new Snake;
+    firstSeg->design = char(254);
+    firstSeg->nextSeg = NULL;
+    firstSeg->prevSeg = NULL;
+    head = firstSeg;
+
     for(int i=0; i<1000; i++)
         {
         map[i] = ' ';
         }
-    map[snakePos] = char(254);
-    cout << map[snakePos] << endl;
+    map[snakePos] = firstSeg->design;
+    map[525] = char(233);
     return;
     }
 
@@ -38,40 +45,17 @@ void MainGame::run()
     int ch;
     while(true)
         {
+        system("cls");
         display();
         ch = getInput();
         Sleep(500);
-        if(ch == KEY_DOWN)
-            {
-            map[snakePos] = ' ';
-            snakePos = snakePos+2;
-            map[snakePos] = char(254);
-            }
-        else if(ch == KEY_UP)
-            {
-            map[snakePos] = ' ';
-            snakePos = snakePos+2;
-            map[snakePos] = char(254);
-            }
-        else if(ch == KEY_LEFT)
-            {
-            map[snakePos] = ' ';
-            snakePos = snakePos+2;
-            map[snakePos] = char(254);
-            }
-        else if(ch == KEY_RIGHT)
-            {
-            map[snakePos] = ' ';
-            snakePos = snakePos+2;
-            map[snakePos] = char(254);
-            }
-        system("cls");
+        movement(ch);
         }
     }
 
 void MainGame::display()
     {
-    //Screen setup
+    //Screen setup 
     int count = 0;
     //top
     cout << ' ';
@@ -100,7 +84,6 @@ void MainGame::display()
         {
         cout << char(196);
         }
-
     }
 
 int MainGame::getInput()
@@ -121,5 +104,86 @@ int MainGame::getInput()
         }
     }
 
+void MainGame::movement(int ch)
+    {
+    if(ch == KEY_DOWN)
+        {
+        if(map[snakePos] == char(233))
+            {
+            collision();
+            }
+        else
+            {
+            map[snakePos] = ' ';
+            }
+        snakePos = snakePos+mapWidth;
+        map[snakePos] = char(254);
+        direction = mapWidth;
+        }
+    else if(ch == KEY_UP)
+        {
+        if(map[snakePos] == char(233))
+            {
+            collision();
+            }
+        else
+            {
+            map[snakePos] = ' ';
+            }
+        
+        snakePos = snakePos-mapWidth;
+        map[snakePos] = char(254);
+        direction = -mapWidth;
+        }
+    else if(ch == KEY_LEFT)
+        {
+        if(map[snakePos] == char(233))
+            {
+            collision();
+            }
+        else
+            {
+            map[snakePos] = ' ';
+            }
+        snakePos = snakePos-2;
+        map[snakePos] = char(254);
+        direction = -2;
+        }
+    else if(ch == KEY_RIGHT)
+        {
+        if(map[snakePos] == char(233))
+            {
+            collision();
+            }
+        else
+            {
+            map[snakePos] = ' ';
+            }
+        snakePos = snakePos+2;
+        map[snakePos] = char(254);
+        direction = 2;
+        }
+    else
+        {
+        if(map[snakePos] == char(233))
+            {
+            collision();
+            }
+        else
+            {
+            map[snakePos] = ' ';
+            }
+        snakePos = snakePos + direction;
+        map[snakePos] = char(254); 
+        } 
+    }
+
+void MainGame::collision()
+    {
+    Snake *newSeg = new Snake;
+    newSeg->nextSeg = head;
+    char design = newSeg->design = char(254);
+    map[snakePos] = design;
+    }
 
 
--- game.h
+++ game.h
@@ -3,6 +3,12 @@
 #include <Windows.h>
 
 #pragma once
+struct Snake
+    {
+    Snake *nextSeg;
+    Snake *prevSeg;
+    char design;
+    };
 
 class MainGame
     {
@@ -13,11 +19,14 @@ class MainGame
         void run();
         void display();
         int getInput();
+        void movement(int ch);
+        void collision();
 
     private:
+        Snake *head;
         int snakePos;
         int mapHeight;
         int mapWidth;
         char *map;
-
+        int direction;
     };
